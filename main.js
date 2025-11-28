8b3ccb6659571d736b8e7d3ec2372a7d0e65ada9
ApolloDAstronaut <apollodastronaut@gmail.com>
2025-11-28 14:34:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+initRenderer();
+
+const worker = new Worker("workers/simulationWorker.js");
+let latestFrame = null;
+
+worker.onmessage = e => latestFrame = e.data;
+
+initUI(worker);
+
+(function loop() {
+  requestAnimationFrame(loop);
+  if (latestFrame) draw(latestFrame);
+})();
 
