c19b187677a70ad0378283b17744ce1c45217872
ApolloDAstronaut <apollodastronaut@gmail.com>
2025-11-28 14:32:58 +0200

    Update isotopes.js

--- physics/isotopes.js
+++ physics/isotopes.js
@@ -1,1 +1,13 @@
+const ISOTOPES = {
+  H:    { Z: 1, A: 1, r: 4,  color: "skyblue" },
+  D:    { Z: 1, A: 2, r: 4.5, color: "lightblue" },
+  T:    { Z: 1, A: 3, r: 5,  color: "#99d" },
+  He:   { Z: 2, A: 4, r: 6,  color: "yellow" },
+
+  U235: { Z: 92, A: 235, r: 10, color: "#8f8" },
+  U238: { Z: 92, A: 238, r: 10, color: "#6f6" },
+  Pu239:{ Z: 94, A: 239, r: 11, color: "#afa" },
+
+  n:    { Z: 0, A: 1, r: 3,  color: "#fff" }
+};
 
